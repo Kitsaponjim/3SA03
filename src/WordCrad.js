745528ee19515c704c0cfe90ca330c4da21fe373
Kitsaponjim <6310110037@psu.ac.th>
2022-08-16 22:35:51 +0700

    PART 5

--- src/WordCrad.js
+++ src/WordCrad.js
@@ -2,9 +2,12 @@ import React from "react";
 import CharacterCard from "./CharacterCard";
 
 export default function WordCrad(props) {
+
+    const activationHandler = c =>{ console.log(`${c} has been acticated.`)}
+
     return (
         <div>
-            {Array.from(props.value).map((c, i) =><CharacterCard value={c} key={i}/>)}
+            {Array.from(props.value).map((c, i) =><CharacterCard value={c} key={i} activationHandler={activationHandler}/>)}
         </div>
     )
 }
