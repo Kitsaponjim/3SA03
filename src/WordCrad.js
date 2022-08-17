4f2f4f100449df289e430c5b5c3cdf082050269a
Kitsaponjim <6310110037@psu.ac.th>
2022-08-18 00:40:52 +0700

    Part 6

--- src/WordCrad.js
+++ src/WordCrad.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import CharacterCard from "./CharacterCard";
+import _, { attempt } from "lodash";
+
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
+    }
+}
 
 export default function WordCrad(props) {
 
-    const activationHandler = c =>{ console.log(`${c} has been acticated.`)}
+    const [state, setState] = useState(prepareStateFromWord(props.value))
+
+    const activationHandler = c =>{ 
+        console.log(`${c} has been activated.`)
+            let guess = state.guess + c
+            setState({...state, guess})
+
+            if(guess.length == state.word.length){
+                if(guess == state.word){
+                    console.log('yeah!')
+                    setState({...state, guess:'', completed:true})
+                }else {
+                    console.log('reset')
+                    setState({...state, guess:'', attempt: state.attempt + 1})
+                }
+            }
+        }
+    
 
     return (
         <div>
-            {Array.from(props.value).map((c, i) =><CharacterCard value={c} key={i} activationHandler={activationHandler}/>)}
+            {state.chars.map((c, i) =><CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>)}
         </div>
     )
 }
