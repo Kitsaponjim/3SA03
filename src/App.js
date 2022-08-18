6f128fb9f73bc1218ca72ac1afd82a5437c6d89e
Kitsaponjim <6310110037@psu.ac.th>
2022-08-19 01:24:40 +0700

    report

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCrad from './WordCrad';
 
-const word = "Hello";
+const word = "youtube";
 function App() {
   return (
     <div>
